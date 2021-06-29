12c3c48b876d4382470c03a9f314ad0c2791a50a
vinuthavasukeesha <vinutha.vasukeesha@lucidatechnologies.com>
2021-06-30 03:21:43 +0530

    Added Delete Confirm box and Altered json data to display user details in right column

--- src/Test2.js
+++ src/Test2.js
@@ -10,18 +10,17 @@ const Test2 = () => {
     const [userData, getuserData] = useState([]);
     const [gettemp, settemp] = useState([]);
     const [filteruserData, filtergetuserData] = useState({});
-    const [isError, setError] = useState("")
+    const [userDetails, setUserDetails] = useState("")
+  
 
 
     useEffect(() => {
-
-        // axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
         axios.get('data/userdata.json').then((res) => {
             console.log(res['data'])
             getuserData(res['data']);
             settemp(res['data'])
         }).catch((e) => {
-
+            console.log('e', e)
         })
     }, []);
 
@@ -39,21 +38,21 @@ const Test2 = () => {
 
         }
 
-        if (value !== temp) {
-            return setError("No records found")
-        }
-
     }
 
     // method to handle selected item in the list 
 
     const HandleSelection = (id, e) => {
         console.log("id", id);
+        //  changeState({...appState,activeObjects:appState.getuserData[id]})
+        //   console.log('changeState',changeState)
         let temp = userData.find((resp) => {
             return resp.id === id;
         });
-        console.log("tempppp", temp)
+        console.log("tempppp", temp.person)
+        setUserDetails(temp.person)
         filtergetuserData(temp);
+        console.log("get person", filteruserData)
     }
 
 
@@ -64,41 +63,58 @@ const Test2 = () => {
         let temp = userData.filter((res) => {
             return res.id !== id;
         });
+        // alert('sure you want to delete')
         getuserData(temp);
 
     }
 
+   
 
     return (
+
         <div class="flex-container">
             <div class="flex-left">
                 <div class="search">
                     <span class="fa fa-search"></span>
-                    <input placeholder="Search..." onChange={HandleInput}/>
+                    <input placeholder="Search..." onChange={HandleInput} />
                 </div>
-                {/* <input type="text" onChange={HandleInput} /> */}
+             
                 <div className="flex-center">
                     {userData.length > 0 ? (
                         userData?.map((resp, i) => {
                             return (<div key={i} className="contents list" >
                                 <div>
-                                    <i class="fa fa-trash" style={{ float: 'right' }} onClick={(e) => HandleDelete(e, resp.id)}></i>
-                                    {/* <button className="delete" onClick={(e) => HandleDelete(e, resp.id)}>DELETE</button> */}
+                                    <i class="fa fa-trash" style={{ float: 'right' }}
+                                        // delete button with js confirm box
+                                        onClick={(e) => {
+                                            if (window.confirm("Delete the user with id " + resp.id)) {
+                                                HandleDelete(e, resp.id)
+                                            }
+                                        }
+                                        }>
+
+                                    </i>
                                 </div>
                                 <ul id="menulist">
-                                    <li className="menuitems"   onClick={(e) => HandleSelection(resp.id, e)}><a>{resp.title}</a></li>
+                                    <li className="menuitems" onClick={(e) => HandleSelection(resp.id, e)}><a>{resp.title}</a></li>
                                 </ul>
                             </div>)
                         })
                     ) : (
-                        <div  style={{ fontSize: '12px', fontFamily: 'serif', justifyContent: 'center', alignItems:'center', marginTop: '20px' }}>
+                        <div style={{ fontSize: '12px', fontFamily: 'serif', justifyContent: 'center', alignItems: 'center', marginTop: '20px' }}>
                             No results found
                         </div>
                     )}
                 </div>
             </div>
+           {/* to display user details */}
             <div class="flex-right right-content">
-                {filteruserData?.body}
+                {
+                    Object.entries(filteruserData).map(([key, val]) =>
+                        <h2 key={key}>{key}: {val}</h2>
+                    )
+                }
+                
             </div>
         </div>
     )
